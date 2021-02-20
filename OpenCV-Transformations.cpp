9fc2fb0ed43865c4397b374900d0ac2d3f2ae915
amishbisaria <62402367+amishbisaria@users.noreply.github.com>
2021-02-20 12:26:24 -0600

    put in waitkey()

--- OpenCV-Transformations.cpp
+++ OpenCV-Transformations.cpp
@@ -106,24 +106,24 @@ int main()
 	Mat doubleSizeImage = scale(image1, doubleSizeMatrix);
 	imshow("Original Image", image1);
 	imshow("Double Size Image", doubleSizeImage);
-	//waitKey(0);
+	waitKey(0);
 
 	//rotate image by 45 degrees and show rotated image
 	float rotateMatrix[3][3] = { {cosf(45 * PI / 180),-sinf(45 * PI / 180),0},{sinf(45 * PI / 180),cosf(45 * PI / 180),0},{0,0,1} };
 	Mat rotateImage = rotate(image1, rotateMatrix);
 	imshow("Rotated Image", rotateImage);
-	//waitKey(0);
+	waitKey(0);
 
 	//translate x by 100 and y by 50 and show translated image
 	int translateMatrix[3][3] = { {1,0,100},{0,1,50},{0,0,1} };
 	Mat translateImage = translate(image1, translateMatrix);
 	imshow("Translated Image", translateImage);
-	//waitKey(0);
+	waitKey(0);
 
 	//translate and then rotate image
 	Mat tranRot = rotate(translateImage, rotateMatrix);
 	imshow("Translated and then Rotated Image", tranRot);
-	//waitKey(0);
+	waitKey(0);
 
 	//rotate and then translate image
 	Mat rotTran = translate(rotateImage, translateMatrix);
